ede1648938608aa5533c0603d3fcb3832630cc9e
Simon Schödler <simon.schoedler@hotmail.ch>
2021-01-29 19:26:27 +0100

    Rm'd init's from project-manager

--- app-project-manager/src/classes/employee.ts
+++
@@ -1,9 +0,0 @@
-import { IReference } from "./resource";
-
-export class Employee implements IReference {
-    public title: string;
-    
-    constructor(_title : string) {
-        this.title = _title;
-    }
-}
